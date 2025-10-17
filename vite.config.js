ba27590c6936ddcd49eddc309ddfb566681f78f5
Nicolás Reyes <nreyes.dev@gmail.com>
2025-10-17 17:18:30 -0300

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// If your repo is https://github.com/yourname/my-landing,
+// set base to '/my-landing/'.
+// If it's yourname.github.io, use base: '/' instead.
 export default defineConfig({
   plugins: [react()],
   base: '/landing-page-demo/',
+  build: { outDir: 'docs' }
 });
+
